3c5e25784f0c7496b1e71ef82bea6f64bbe54a22
Mihir Joshi <mihir67mj@gmail.com>
2020-06-02 17:43:00 +0530

    Project Setup

---
+++ src/test/java/com/dicom/dicomextract/DicomextractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dicom.dicomextract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DicomextractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
